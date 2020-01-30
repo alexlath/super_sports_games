928ee94c5a4c82a3e194eebb6252860b3238d8ce
Alex Latham <alexlthm@gmail.com>
2020-01-30 13:46:18 -0700

    Add Event class

---
+++ lib/event.rb
@@ -0,0 +1,8 @@
+class Event
+
+  def initialize(event_name_parameter, participant_ages_parameter)
+    @event_name = event_name_parameter
+    @participant_ages = participant_ages_parameter
+  end
+
+end
