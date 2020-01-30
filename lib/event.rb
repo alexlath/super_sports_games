a211b140ba2741155be727a95b5399fe036cb44c
Alex Latham <alexlthm@gmail.com>
2020-01-30 16:17:10 -0700

    Complete runner file for user inputs and outputs

--- lib/event.rb
+++ lib/event.rb
@@ -15,19 +15,19 @@ class Event
   end
 
   def average_age
-    @sum = 0
-    @ages.each { |age| @sum += age }
-    @count = ages.count
-    @sum.to_f / @count
+    sum = 0
+    @ages.each { |age| sum += age }
+    count = @ages.count
+    sum.to_f / count
   end
 
   def standard_deviation_age
-    @average = average_age
-    @differences_from_average = @ages.map { |age| age - @average }
-    @differences_squared = @differences_from_average.map { |difference| difference ** 2 }
-    @differences_sq_sum = 0
-    @differences_squared.each { |difference_sq| @differences_sq_sum += difference_sq }
-    @avg_squared_differences = @differences_sq_sum / @count
-    @std_deviation = Math.sqrt(@avg_squared_differences).round(2)
+    average = average_age
+    differences_from_average = @ages.map { |age| age - average }
+    differences_squared = differences_from_average.map { |difference| difference ** 2 }
+    differences_sq_sum = 0
+    differences_squared.each { |difference_sq| differences_sq_sum += difference_sq }
+    avg_squared_differences = differences_sq_sum / @ages.count
+    std_deviation = Math.sqrt(avg_squared_differences).round(2)
   end
 end
--- runner.rb
+++ runner.rb
@@ -1,9 +1,30 @@
+# add required files
 require './lib/event.rb'
 require './lib/games.rb'
 
+# Get the year from the user
 p "Welcome to the Super Sports Games!! What year is it?"
 year = gets.chomp
 
 current_games = Games.new(year)
 
+# User can create new Events and get a Summary of the Events
 p "Wow, is it #{year} already? Let's add some events to this year's games."
+
+answer = "y"
+
+until answer == "n" do
+  p "What is the name of the event?"
+  event_name_parameter = gets.chomp
+
+  p "Hmm, #{event_name_parameter}ing sounds like a tough sport! What are the ages of the participants? (age1, age2, age3...)"
+  participant_ages_parameter = gets.chomp.split(', ').map { |age| age.to_i }
+
+  current_games.add_event(Event.new(event_name_parameter, participant_ages_parameter))
+
+  p "Thanks for the info! Do you want to add another event? (y/n)"
+  answer = gets.chomp
+end
+
+p "Great series of events! Here's a summary:"
+p current_games.all_events_summary
