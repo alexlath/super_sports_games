307ce9f8d2581c6feae89b07475ee2abcdea4944
Alex Latham <alexlthm@gmail.com>
2020-01-30 14:30:50 -0700

    Add ages and max age tests

--- test/event_test.rb
+++ test/event_test.rb
@@ -16,14 +16,19 @@ class GamesTest < Minitest::Test
     assert_equal "Curling", event.name
   end
 
+  def test_it_has_an_ages_array
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+
+    assert_equal [24, 30, 18, 20, 41], event.ages
+  end
+
+  def test_it_can_find_max_age
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+
+    assert_equal 41, event.max_age
+  end
 
 end
-# pry(main)> event.name
-# #=> "Curling"
-#
-# pry(main)> event.ages
-# #=> [24, 30, 18, 20, 41]
-#
 # pry(main)> event.max_age
 # #=> 41
 #
