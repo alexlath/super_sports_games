aecbd50baec381f580672fe0c3735d73bf071889
Alex Latham <alexlthm@gmail.com>
2020-01-30 14:49:21 -0700

    Add functionality to pass a year and create a Games instance

--- runner.rb
+++ runner.rb
@@ -1,2 +1,9 @@
 require './lib/event.rb'
 require './lib/games.rb'
+
+p "Welcome to the Super Sports Games!! What year is it?"
+year = gets.chomp
+
+current_games = Games.new(year)
+
+p "Wow, is it #{year} already? Let's add some events to this year's games."
