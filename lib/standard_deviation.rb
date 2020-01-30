7fd54a49b3ba114c0cd0a22983a01087f120e68a
Alex Latham <alexlthm@gmail.com>
2020-01-30 13:41:51 -0700

    Add standard deviation calculations

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,30 @@
+# Input ages into array
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+# Sum each element (age) in the array (ages)
+sum = 0
+ages.each { |age| sum += age }
 
-# When you find the standard deviation, print it out
+# Count number of age elements in the ages array
+count = ages.count
+
+# Divide sum by count to find average
+average = sum.to_f / count
+
+# Subtract the average from each age
+differences_from_average = ages.map { |age| age - average }
+
+# Square each difference
+differences_squared = differences_from_average.map { |difference| difference ** 2 }
+
+# Sum squared differences
+differences_sq_sum = 0
+differences_squared.each { |difference_sq| differences_sq_sum += difference_sq }
+
+# Divide sum of squared differences by count
+avg_squared_differences = differences_sq_sum / count
+
+# Square root of sum of average of squared differences
+std_deviation = Math.sqrt(avg_squared_differences).round(2)
+
+p std_deviation
